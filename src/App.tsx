7468d29b3ebfece3bf1830a7505c6f535ec76f15
Joshua Brown <jbr242@uclive.ac.nz>
2023-05-26 11:30:28 +1200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="flex min-w-[100vw] min-h-screen items-center justify-center">
+        <h1 className="text-blue-700 text-8xl">
+            joshgbrown
+        </h1>
+    </div>
+  )
+}
+
+export default App
