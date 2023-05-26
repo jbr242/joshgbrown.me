d1181fb0c4b4a1aff4a0f0fa82b42cc8323bb78c
Joshua Brown <jbr242@uclive.ac.nz>
2023-05-26 16:54:25 +1200

    changed loading animation

--- src/views/Loading.tsx
+++ src/views/Loading.tsx
@@ -6,7 +6,7 @@ export default function Loading({ loaded }: LoadingProps) {
   return (
     <div
       className="fixed inset-0 bg-white z-10 flex min-w-[100vw] min-h-screen justify-center items-center text-center
-        data-[loaded=true]:opacity-0 data-[loaded=true]:-translate-y-20 transition-all duration-1000"
+        data-[loaded=true]:opacity-0 data-[loaded=true]:translate-y-40 transition-all duration-1000"
       data-loaded={loaded}
     >
       <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
