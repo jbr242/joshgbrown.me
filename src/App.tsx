662cbea1136fe444a3f9bbde467c61c0881afa6d
Joshua Brown <jbr242@uclive.ac.nz>
2023-05-26 16:40:01 +1200

    implemented loading animation to stop unloadedimg

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
 import Profile from "./views/Profile";
+import Loading from "./views/Loading";
 
 function App() {
-  return <Profile />;
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    setTimeout(() => setLoaded(true), 2000);
+  }, []);
+
+  return (
+    <>
+      <Loading loaded={loaded} />
+      {loaded && <Profile />}
+    </>
+  );
 }
 
 export default App;
